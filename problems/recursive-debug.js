ab45a43204d3654598c17de7a642fbdf7aad1b20
Jacqueline <jgeyfen@gmail.com>
2023-08-26 13:07:18 -0700

    debugger prob done

--- problems/recursive-debug.js
+++ problems/recursive-debug.js
@@ -14,8 +14,13 @@ doForAll(["a", "b", "c"], (x) => x.toUpperCase()); // => ["A", "B", "C"]
 ***********************************************************************/
 
 function doForAll(arr, action) {
-  return [action(arr[0]), ...doForAll(arr.slice(1), action)];
-}
+  if (arr.length === 0) {
+    return [];
+  } else {
+    return [action(arr[0]), ...doForAll(arr.slice(1), action)];
+  }
+
+};
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS LINE*****************/
 module.exports = doForAll;
